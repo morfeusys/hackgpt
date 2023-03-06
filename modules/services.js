425204b8d5fc94335bf86180aa62fa624dac29d2
morfeusys <che@just-ai.com>
2023-03-06 18:48:53 +0300

    disable ip throttling

--- modules/services.js
+++ modules/services.js
@@ -18,10 +18,10 @@ module.exports = async (app) => {
         const type = req.params['type']
         const service = gptServices[type]
         if (service) {
-            if (requests[req.clientIp]) {
-                res.status(403).send('Please await your previous request to complete')
-                return
-            }
+            // if (requests[req.clientIp]) {
+            //     res.status(403).send('Please await your previous request to complete')
+            //     return
+            // }
             requests[req.clientIp] = new Date().getTime()
             try {
                 const prompt = req.query['prompt'] || req.body['prompt']
